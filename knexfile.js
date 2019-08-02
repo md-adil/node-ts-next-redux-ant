5716eba6a0bf9d52c95cc7e6e5a67e23ab0a5c3f
Md Adil <md-adil@live.com>
2019-08-02 15:03:32 +0530

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+require("dotenv").config();
+const config = require("./dist/api/configs/database").default;
+module.exports = {
+    client: "mysql2",
+    connection: {
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        database: config.database
+    },
+    seeds: {
+        directory: "./migrations/seeds"
+    }
+};
